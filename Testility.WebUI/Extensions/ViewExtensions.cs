c62a5fef30af50a120e1a5a91b9af8097faaf0b0
unknown <lozog@LOZOG-RZE.pgs-soft.com>
2015-08-10 08:35:26 +0200

    [Html Helper]

---
+++ Testility.WebUI/Extensions/ViewExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace Testility.WebUI.Extensions
+{
+    public static class ViewExtensions
+    {
+        public static MvcHtmlString SourceCodesStatusHelper(this HtmlHelper helper)
+        {
+        TagBuilder builder =  new TagBuilder("div");
+                if ( helper.ViewContext.Controller.TempData["savemessage"] != null)
+                    {
+                        TagBuilder saveMessage = new TagBuilder("div");
+                        saveMessage.MergeAttribute("class", "alert alert-success");
+                        saveMessage.InnerHtml = helper.ViewContext.Controller.TempData["savemessage"].ToString();
+                        builder.InnerHtml += saveMessage.ToString();   
+                }
+                if (helper.ViewContext.Controller.TempData["errormessage"] != null)
+                    {
+                        TagBuilder errorMessage = new TagBuilder("div");
+                        errorMessage.MergeAttribute("class", "alert alert-danger");
+                        errorMessage.InnerHtml = helper.ViewContext.Controller.TempData["errormessage"].ToString();
+                        builder.InnerHtml += errorMessage.ToString();   
+                    }               
+
+                return MvcHtmlString.Create(builder.ToString());
+        }
+    }
+}
