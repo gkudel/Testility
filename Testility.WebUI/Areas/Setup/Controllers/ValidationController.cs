fa53f2155b1c8d44afef4b212478fb71632d386d
unknown <lozog@LOZOG-RZE.pgs-soft.com>
2015-08-12 09:19:23 +0200

    [Missing Controller ]

---
+++ Testility.WebUI/Areas/Setup/Controllers/ValidationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Testility.Domain.Abstract;
+
+namespace Testility.WebUI.Areas.Setup.Controllers
+{
+    public class ValidationController : Controller
+    {
+        private ISetupRepository setupRepository;
+
+        public ValidationController(ISetupRepository setupRepositor)
+        {
+            setupRepository = setupRepositor;
+        }
+
+        public JsonResult IsNameUnique(string name)
+        {
+            return setupRepository.CheckSourceCodeNameIsUnique(name) ? Json(true, JsonRequestBehavior.AllowGet) : Json(false, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
