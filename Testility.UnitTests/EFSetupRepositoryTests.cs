87295c87173ffce0fec0583524775e59a940eb88
Lukasz <lozog@pgs-soft.com>
2015-08-17 11:33:47 +0200

    [Tests Service]

--- Testility.UnitTests/EFSetupRepositoryTests.cs
+++ Testility.UnitTests/EFSetupRepositoryTests.cs
@@ -1,109 +1,135 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-
 using Testility.Domain.Concrete;
 using Testility.Domain.Entities;
-using Testility.Domain.Mappings;
+
 
 namespace Testility.UnitTests
 {
     [TestClass]
     public class EFSetupRepositoryTests
     {
-
-        public Mock<DbSet<Item>> MockSet { get; set; }
-        public Mock<DbSet<Class>> MockTested { get; set; }
-        public Mock<DbSet<Method>> MockMethod { get; set; }
-        public Mock<DbSet<Test>> MockTest { get; set; }
-
+        #region Members
         public Mock<EFDbContext> MockContext { get; set; }
-        public EFSetupRepository Service { get; set; }
 
+        public Mock<DbSet<Solution>> MockSolution { get; set; }
+
+        public EFSetupRepository Service { get; set; }
+        #endregion
+
+
+        #region Init
         [TestInitialize]
         public void Init()
         {
-            var data = new List<Item> 
-            { 
-                new Item() {Id = 1, Name = "ok"},
+            var SolutionsData = new List<Solution>
+            {
+                new Solution() {Id = 2, Name = "12ok"},
+                new Solution() {Id = 1, Name = "1"}
+            };
+
+            var ItemsData = new List<Item>
+            {
                 new Item() {Id = 2, Name = "12ok"}
+            };
 
-            }.AsQueryable();
+            var ClassesData = new List<Class>
+            {
+                new Class() {Id = 2, Name = "12ok"}
+            };
 
-            MockSet = new Mock<DbSet<Item>>();
-            MockSet.As<IQueryable<Item>>().Setup(x => x.Provider).Returns(data.Provider);
-            MockSet.As<IQueryable<Item>>().Setup(x => x.Expression).Returns(data.Expression);
-            MockSet.As<IQueryable<Item>>().Setup(x => x.ElementType).Returns(data.ElementType);
-            MockSet.As<IQueryable<Item>>().Setup(x => x.GetEnumerator()).Returns(data.GetEnumerator);
+            var MethodsData = new List<Method>
+            {
+                new Method() {Id = 2, Name = "12ok"}
+            };
 
-
-            var testedData = new List<Class> { }.AsQueryable();
-
-            MockTested = new Mock<DbSet<Class>>();
-            MockTested.As<IQueryable<Class>>().Setup(x => x.Provider).Returns(testedData.Provider);
-            MockTested.As<IQueryable<Class>>().Setup(x => x.Expression).Returns(testedData.Expression);
-            MockTested.As<IQueryable<Class>>().Setup(x => x.ElementType).Returns(testedData.ElementType);
-            MockTested.As<IQueryable<Class>>().Setup(x => x.GetEnumerator()).Returns(testedData.GetEnumerator);
-
-            var methodData = new List<Method> { }.AsQueryable();
-
-            MockMethod = new Mock<DbSet<Method>>();
-            MockMethod.As<IQueryable<Method>>().Setup(x => x.Provider).Returns(methodData.Provider);
-            MockMethod.As<IQueryable<Method>>().Setup(x => x.Expression).Returns(methodData.Expression);
-            MockMethod.As<IQueryable<Method>>().Setup(x => x.ElementType).Returns(methodData.ElementType);
-            MockMethod.As<IQueryable<Method>>().Setup(x => x.GetEnumerator()).Returns(methodData.GetEnumerator);
-
-
-            var testData = new List<Test> { }.AsQueryable();
-
-            MockTest = new Mock<DbSet<Test>>();
-            MockTest.As<IQueryable<Test>>().Setup(x => x.Provider).Returns(testData.Provider);
-            MockTest.As<IQueryable<Test>>().Setup(x => x.Expression).Returns(testData.Expression);
-            MockTest.As<IQueryable<Test>>().Setup(x => x.ElementType).Returns(testData.ElementType);
-            MockTest.As<IQueryable<Test>>().Setup(x => x.GetEnumerator()).Returns(testData.GetEnumerator);
+            var TestsData = new List<Test>
+            {
+                new Test() {Id = 2, Name = "12ok"}
+            };
 
             MockContext = new Mock<EFDbContext>();
 
-            MockContext.Setup(x => x.SourCodes).Returns(MockSet.Object);
-            MockContext.Setup(x => x.TestedClasses).Returns(MockTested.Object);
-            MockContext.Setup(x => x.TestedMethods).Returns(MockMethod.Object);
-            MockContext.Setup(x => x.Tests).Returns(MockTest.Object);
+
+            MockSolution = GetQueryableMockDbSet(SolutionsData);
+            MockContext.Setup(x => x.Solutions).Returns(MockSolution.Object);
+
+            MockContext.Setup(x => x.Items).Returns(GetQueryableMockDbSet(ItemsData).Object);
+            MockContext.Setup(x => x.Classes).Returns(GetQueryableMockDbSet(ClassesData).Object);
+            MockContext.Setup(x => x.Methods).Returns(GetQueryableMockDbSet(MethodsData).Object);
+            MockContext.Setup(x => x.Tests).Returns(GetQueryableMockDbSet(TestsData).Object);
 
             Service = new EFSetupRepository(MockContext.Object);
         }
 
-        [TestMethod]
-        public void Can_Get_All_Source_Codes()
+        #endregion
+
+
+
+        #region Supp
+        private Mock<DbSet<T>> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
         {
-            Assert.AreEqual(2, Service.GetAllSourceCodes().Count());
+            var queryable = sourceList.AsQueryable();
+
+            var dbSet = new Mock<DbSet<T>>();
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+
+            return dbSet;
+        }
+        #endregion
+
+        [TestMethod]
+        public void Can_Get_All_Solutions()
+        {
+            Assert.AreEqual(2, Service.GetSolutions().Count());
         }
 
         [TestMethod]
-        public void Cannot_Delete_Source_Code_With_WrongId()
+        public void Can_Get_Single_Solution()
+        {
+            Assert.AreEqual("1", Service.GetSolution(1).Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.NullReferenceException))]
+        public void Cannot_Get_Single_Solution()
+        {
+            Assert.AreEqual(null, Service.GetSolution(10).Name);
+        }
+
+        [TestMethod]
+        public void Can_Add_Solution()
+        {
+            Solution solution = new Solution() { Name = "ok" };
+            Service.Save(solution);
+            MockSolution.Verify(x => x.Add(It.IsAny<Solution>()), Times.Once);
+            MockContext.Verify(x => x.SaveChanges(), Times.Once);
+        } 
+
+        [TestMethod]
+        public void Cannot_DeleteSolutionWithWrongId()
         {
             var result = Service.Delete(10);
-            MockSet.Verify(m => m.Remove(It.IsAny<Item>()), Times.Never);
+
+            MockSolution.Verify(m => m.Remove(It.IsAny<Solution>()), Times.Never);
             MockContext.Verify(m => m.SaveChanges(), Times.Never);
             Assert.AreEqual(false, result);
         }
 
         [TestMethod]
-        public void Can_Delete_Source_Code()
+        public void Can_DeleteSolution()
         {
-            Service.Delete(1);
-            MockSet.Verify(m => m.Remove(It.IsAny<Item>()), Times.Once);
-            MockContext.Verify(m => m.SaveChanges(), Times.Once);
-        }
+            var result = Service.Delete(1);
 
-        [TestMethod]
-        public void Cannot_GetSourceCode_NullReturn()
-        {
-            var result = Service.GetSourceCode(null);
-            Assert.AreEqual(null,result);
+            MockSolution.Verify(m => m.Remove(It.IsAny<Solution>()), Times.Once);
+            MockContext.Verify(m => m.SaveChanges(), Times.Once);
+            Assert.AreEqual(true, result);
         }
     }
 }
