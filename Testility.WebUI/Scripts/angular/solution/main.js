6a14e5830beb297335c40f4dd8b1660bfeae69ea
Łukasz Ożóg <lozog@pgs-soft.com>
2015-08-18 15:56:19 +0200

    [Angular Cd]

--- Testility.WebUI/Scripts/angular/solution/main.js
+++ Testility.WebUI/Scripts/angular/solution/main.js
@@ -1,14 +1,7 @@
 ﻿var solutionApp = angular.module("solutionApp", []);
 
 solutionApp.controller("ReferenceController", ['$scope', '$http', function ($scope, $http) {
-    $scope.ReferencesClick = function (id) {
-        if ($scope.References === undefined) {
-            $http.get('/References/GetListOfReferences/' + id, 'content.json').then(function (response) {
-                $scope.References = response.data.AvailableReferences;
-                $scope.SolutionReferences = response.data.SelectedReferences;
-            });
-        }
-    }
+
     $scope.Selected = function (id) {
         var returnValue = false;
         angular.forEach($scope.SolutionReferences, function (item, key) {
@@ -19,4 +12,32 @@ solutionApp.controller("ReferenceController", ['$scope', '$http', function ($sco
         });
         return returnValue;
     };
+   
+    $scope.ReferencesClick = function (id) {
+        if ($scope.References === undefined) {
+            $http.get('/References/GetListOfReferences/' + id, 'content.json').then(function (response) {
+                $scope.References = response.data.AvailableReferences;
+                $scope.SolutionReferences = response.data.SelectedReferences;
+            });
+        }
+    }
+
+    $scope.showData = function () {
+        $scope.curPage = 0;
+        $scope.pageSize = 5;
+        $scope.numberOfPages = function () {
+            return Math.ceil($scope.References.length / $scope.pageSize);
+        };
+
+    }
+
 }]);
+
+
+angular.module('solutionApp').filter('pagination', function () {
+    return function (input, start) {
+        start = +start;
+        return input.slice(start);
+    };
+});
+
